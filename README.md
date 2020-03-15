8257739c82abde36bd5be605b391e05ba9bafa03
Tugkan Cengiz <tugkancengiz@gmail.com>
2020-03-15 13:58:54 +0300

    ready for prod

---
+++ README.md
@@ -0,0 +1,7 @@
+# Coronavirus stats in Luxembourg
+Actor gets an actual number of infected and deceased people in Luxembourg from https://gouvernement.lu/fr/dossiers.gouv_msan+fr+dossiers+2020+corona-virus.html#bloub-0
+
+Latest data are available at this URL: https://api.apify.com/v2/key-value-stores/3Po6TV7wTht4vIEid/records/LATEST?disableRedirect=true
+You can find a dataset with unique history items here https://api.apify.com/v2/datasets/L3VCmhMeX0KUQeJto/items?format=json&clean=1
+
+URL is being actualized every 5 minutes.
---
+++ src/main.js
@@ -0,0 +1,61 @@
+const Apify = require('apify');
+
+const { log } = Apify.utils;
+const sourceUrl = 'https://gouvernement.lu/fr/dossiers.gouv_msan+fr+dossiers+2020+corona-virus.html#bloub-0';
+const LATEST = 'LATEST';
+
+Apify.main(async () => {
+    const requestQueue = await Apify.openRequestQueue();
+    const kvStore = await Apify.openKeyValueStore('COVID-19-LUXEMBOURG');
+    const dataset = await Apify.openDataset('COVID-19-LUXEMBOURG-HISTORY');
+
+    await requestQueue.addRequest({ url: sourceUrl });
+    const crawler = new Apify.CheerioCrawler({
+        requestQueue,
+        useApifyProxy: true,
+        apifyProxyGroups: ['SHADER'],
+        handlePageTimeoutSecs: 60 * 2,
+        handlePageFunction: async ({ $ }) => {
+            log.info('Page loaded.');
+            const now = new Date();
+
+            $('body strong').remove(); // remove bold title from confirmed cases
+            const h3 = $('.table--container h3');
+            const cases = $(h3[0]);
+            const infected = parseInt(cases.text().match(/\d+/)[0], 10);
+            const deceased = parseInt($(h3[1]).text().match(/\d+/)[0], 10);
+
+            const data = {
+                infected,
+                deceased,
+                sourceUrl,
+                lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, now.getMinutes())).toISOString(),
+                readMe: 'https://apify.com/tugkan/covid-lu',
+            };
+
+            // Compare and save to history
+            const latest = await kvStore.getValue(LATEST) || {};
+            delete latest.lastUpdatedAtApify;
+            const actual = Object.assign({}, data);
+            delete actual.lastUpdatedAtApify;
+
+            if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+                log.info('Data did change :( storing new to dataset.');
+                await dataset.pushData(data);
+            }
+
+            await kvStore.setValue(LATEST, data);
+            log.info('Data stored, finished.');
+        },
+
+        // This function is called if the page processing failed more than maxRequestRetries+1 times.
+        handleFailedRequestFunction: async ({ request }) => {
+            console.log(`Request ${request.url} failed twice.`);
+        },
+    });
+
+    // Run the crawler and wait for it to finish.
+    await crawler.run();
+
+    console.log('Crawler finished.');
+});
